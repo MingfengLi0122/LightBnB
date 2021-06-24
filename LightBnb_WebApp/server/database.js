ac75695feeee17f5e018df5691a88ac0b0690b28
Mingfeng Li <lmf950122@gmail.com>
2021-06-24 03:22:41 +0000

    Update functions with SQL parameterized queriesto extract or insert data to the lightbnb database

---
+++ LightBnb_WebApp/server/database.js
@@ -0,0 +1,169 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+/// Users
+
+/**
+ * Get a single user from the database given their email.
+ * @param {String} email The email of the user.
+ * @return {Promise<{}>} A promise to the user.
+ */
+const getUserWithEmail = function(email) {
+  const queryStr = `
+    SELECT * FROM users
+    WHERE users.email = $1;  
+  `;
+  const value = [email];
+  return pool
+    .query(queryStr, value)
+    .then(res => {
+      if(res.rows) {
+        return res.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch(err => console.error("Query error:", err));
+}
+
+/**
+ * Get a single user from the database given their id.
+ * @param {string} id The id of the user.
+ * @return {Promise<{}>} A promise to the user.
+ */
+const getUserWithId = function(id) {
+  const queryStr = `
+    SELECT * FROM users
+    WHERE users.id = $1
+  `;
+  const value = [id];
+  return pool
+    .query(queryStr, value)
+    .then(res => {
+      if (res.rows) {
+        return res.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch(err => console.error("Query error:", err));
+}
+
+/**
+ * Add a new user to the database.
+ * @param {{name: string, password: string, email: string}} user
+ * @return {Promise<{}>} A promise to the user.
+ */
+const addUser =  function(user) {
+  const queryStr = `
+    INSERT INTO users(name, email, password)
+    VALUES($1, $2, $3)
+    RETURNING *;
+  `;
+  const value = [user.name, user.password, user.email];
+  return pool
+    .query(queryStr, value)
+    .then(res => {
+      if(res.rows) {
+        return res.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch(err => console.error("Query error:", err));
+}
+
+/// Reservations
+
+/**
+ * Get all reservations for a single user.
+ * @param {string} guest_id The id of the user.
+ * @return {Promise<[{}]>} A promise to the reservations.
+ */
+const getAllReservations = function(guest_id, limit = 10) {
+  const queryStr = `
+    SELECT reservations.*,
+           properties.*,
+           AVG(rating) AS average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON reservations.id = reservation_id 
+                          AND properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1 AND end_date < now()::date
+    GROUP BY reservations.id, properties.id
+    ORDER BY start_date
+    LIMIT $2;
+  `;
+  const value = [guest_id, limit];
+  return pool
+    .query(queryStr, value)
+    .then(res => {
+      if(res.rows) {
+        return res.rows;
+      } else {
+        return null;
+      }
+    })
+    .catch(err => console.error("Query error:", err));
+}
+
+/// Properties
+
+/**
+ * Get all properties.
+ * @param {{}} options An object containing query options.
+ * @param {*} limit The number of results to return.
+ * @return {Promise<[{}]>}  A promise to the properties.
+ */
+const getAllProperties = function(options, limit = 10) {
+  const queryStr = `SELECT * FROM properties LIMIT $1`;
+  const value = [limit];
+  return pool
+    .query(queryStr, value)
+    .then(res => {
+      if(res.rows) {
+        return res.rows;
+      } else {
+        return null;
+      }
+    })
+    .catch(err => console.error("Query error:", err));
+}
+
+/**
+ * Add a property to the database
+ * @param {{}} property An object containing all of the property details.
+ * @return {Promise<{}>} A promise to the property.
+ */
+const addProperty = function(property) {
+  //property.active = true;
+  const queryStr = `
+  INSERT INTO properties(owner_id, title , description, thumbnail_photo_url, cover_photo_url,cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+  VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
+  RETURNING *;
+  `;
+  const value = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code, true];
+  return pool
+    .query(queryStr, value)
+    .then(res => {
+      if(res.rows) {
+        return res.rows;
+      } else {
+        return null;
+      }
+    })
+    .catch(err => console.error("Query error:", err));
+}
+
+exports.getUserWithEmail = getUserWithEmail;
+exports.getUserWithId = getUserWithId;
+exports.addUser = addUser;
+exports.getAllReservations = getAllReservations;
+exports.getAllProperties = getAllProperties;
+exports.addProperty = addProperty;
